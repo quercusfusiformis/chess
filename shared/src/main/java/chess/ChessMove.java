b640b3233a2026e42cd7d32909aaf248c780fe32
quercusfursiformis <sricks393@gmail.com>
2024-01-21 21:00:38 -0700

    Re-unzipped starter code to get correct project source code settings

---
+++ shared/src/main/java/chess/ChessMove.java
@@ -0,0 +1,59 @@
+package chess;
+
+import java.util.Objects;
+
+/**
+ * Represents moving a chess piece on a chessboard
+ * <p>
+ * Note: You can add to this class, but you may not alter
+ * signature of the existing methods.
+ */
+public class ChessMove {
+    private ChessPosition start;
+    private ChessPosition end;
+    private ChessPiece.PieceType promotionPiece;
+
+    public ChessMove(ChessPosition startPosition, ChessPosition endPosition,
+                     ChessPiece.PieceType promotionPiece) {
+        this.start = startPosition;
+        this.end = endPosition;
+        this.promotionPiece = promotionPiece;
+    }
+
+    /**
+     * @return ChessPosition of starting location
+     */
+    public ChessPosition getStartPosition() {
+        return this.start;
+    }
+
+    /**
+     * @return ChessPosition of ending location
+     */
+    public ChessPosition getEndPosition() {
+        return this.end;
+    }
+
+    /**
+     * Gets the type of piece to promote a pawn to if pawn promotion is part of this
+     * chess move
+     *
+     * @return Type of piece to promote a pawn to, or null if no promotion
+     */
+    public ChessPiece.PieceType getPromotionPiece() {
+        return this.promotionPiece;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ChessMove chessMove = (ChessMove) o;
+        return Objects.equals(start, chessMove.start) && Objects.equals(end, chessMove.end) && promotionPiece == chessMove.promotionPiece;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(start, end, promotionPiece);
+    }
+}
