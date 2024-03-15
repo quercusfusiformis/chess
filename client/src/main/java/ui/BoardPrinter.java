6f5ab4acbd1250a73c4ace7748d3f6714d1d304e
quercusfursiformis <sricks393@gmail.com>
2024-03-15 03:16:27 -0600

    Fixed coloration bug. Refactored for readability and maintainability. Still some refactoring to do.

--- client/src/main/java/ui/BoardPrinter.java
+++ client/src/main/java/ui/BoardPrinter.java
@@ -3,26 +3,40 @@ package ui;
 import static ui.EscapeSequences.*;
 import java.io.PrintStream;
 import java.nio.charset.StandardCharsets;
-
 import chess.ChessGame;
 import chess.ChessBoard;
 import chess.ChessPiece;
 import chess.ChessPosition;
 
-public class BoardPresenter {
+public class BoardPrinter {
     public static ChessBoard defaultBoard = new ChessBoard();
     static {
         defaultBoard.resetBoard();
     }
 
+    private static final String boardEdgeColor = SET_BG_COLOR_BROWN;
+    private static final String boardSpaceWhiteColor = SET_BG_COLOR_LIGHT_GREY;
+    private static final String boardSpaceBlackColor = SET_BG_COLOR_DARK_GREY;
+    private static final String boardSeparatorColor = RESET_BG_COLOR;
+
+    public static void printBoardAll(ChessBoard board) {
+        printBoard(board, ChessGame.TeamColor.BLACK);
+        printBlankFullRow(System.out, boardSeparatorColor);
+        printBoard(board, ChessGame.TeamColor.WHITE);
+    }
+
     public static void printBoard(ChessBoard board, ChessGame.TeamColor callerColor) {
         PrintStream out = new PrintStream(System.out, true, StandardCharsets.UTF_8);
+
+        int spaceColorDesignator;
         int [] rowOrder;
         int [] colOrder;
         if (callerColor == ChessGame.TeamColor.WHITE) {
+            spaceColorDesignator = 0;
             rowOrder = new int [] {8,7,6,5,4,3,2,1};
             colOrder = new int [] {1,2,3,4,5,6,7,8};
         } else {
+            spaceColorDesignator = 1;
             rowOrder = new int [] {1,2,3,4,5,6,7,8};
             colOrder = new int [] {8,7,6,5,4,3,2,1};
         }
@@ -30,37 +44,34 @@ public class BoardPresenter {
         printHeader(out, callerColor);
         ChessGame.TeamColor currSpaceColor;
         for(int i: rowOrder) {
-            out.print(SET_BG_COLOR_BROWN);
+            out.print(boardEdgeColor);
             out.print(SET_TEXT_COLOR_BLACK);
             printCharWithSpacing(out, String.valueOf(i));
-            if (i % 2 == 1) { currSpaceColor = ChessGame.TeamColor.BLACK; }
-            else { currSpaceColor = ChessGame.TeamColor.WHITE; }
+            if (i % 2 == spaceColorDesignator) { currSpaceColor = ChessGame.TeamColor.WHITE; }
+            else { currSpaceColor = ChessGame.TeamColor.BLACK; }
             for (int j: colOrder) {
                 if (currSpaceColor == ChessGame.TeamColor.WHITE) {
-                    out.print(SET_BG_COLOR_LIGHT_GREY);
+                    out.print(boardSpaceWhiteColor);
                     currSpaceColor = ChessGame.TeamColor.BLACK;
                 } else {
-                    out.print(SET_BG_COLOR_DARK_GREY);
+                    out.print(boardSpaceBlackColor);
                     currSpaceColor = ChessGame.TeamColor.WHITE;
                 }
                 ChessPiece currPiece = board.getPiece(new ChessPosition(i, j));
                 if (!(currPiece == null)) { printPiece(out, currPiece.getPieceType(), currPiece.getTeamColor()); }
                 else { out.print(CHESSSPACE); }
             }
-            out.print(SET_BG_COLOR_BROWN);
+            out.print(boardEdgeColor);
             out.print(CHESSSPACE);
             out.print(RESET_BG_COLOR);
             out.print(System.lineSeparator());
         }
-        out.print(SET_BG_COLOR_BROWN);
-        for (int i = 0; i <= 9; i++) { out.print(CHESSSPACE); }
-        out.print(RESET_BG_COLOR);
-        out.print(System.lineSeparator());
+        printFooter(out);
     }
 
     private static void printHeader(PrintStream out, ChessGame.TeamColor callerColor) {
         String [] headerOrder;
-        out.print(SET_BG_COLOR_BROWN);
+        out.print(boardEdgeColor);
         out.print(CHESSSPACE);
         out.print(SET_TEXT_COLOR_BLACK);
         if (callerColor == ChessGame.TeamColor.WHITE) {
@@ -70,6 +81,17 @@ public class BoardPresenter {
             printCharWithSpacing(out, headerLetter);
         }
         out.print(CHESSSPACE);
+        out.print(RESET_BG_COLOR);
+        out.print(System.lineSeparator());
+    }
+
+    private static void printFooter(PrintStream out) {
+        printBlankFullRow(out, boardEdgeColor);
+    }
+
+    private static void printBlankFullRow(PrintStream out, String color) {
+        out.print(color);
+        for (int i = 0; i <= 9; i++) { out.print(CHESSSPACE); }
         out.print(RESET_BG_COLOR);
         out.print(System.lineSeparator());
     }
---
+++ client/src/test/java/boardPrinterTests/BoardPrinterTest.java
@@ -0,0 +1,21 @@
+package boardPrinterTests;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import ui.BoardPrinter;
+import chess.ChessGame;
+
+class BoardPrinterTest {
+    @BeforeEach
+    void setUp() {}
+
+    @Test
+    void printBoardDefault() {
+        BoardPrinter.printBoard(BoardPrinter.defaultBoard, ChessGame.TeamColor.BLACK);
+    }
+
+    @Test
+    void printBoardAllDefault() {
+        BoardPrinter.printBoardAll(BoardPrinter.defaultBoard);
+    }
+}
