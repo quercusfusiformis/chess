fcc528de08a54047010b807d4af4b4ebc2a7050a
quercusfursiformis <sricks393@gmail.com>
2024-04-09 23:53:40 -0600

    Stubbed out websocket backend functions.

--- server/src/main/java/websocketHandlers/WebsocketServices.java
+++ server/src/main/java/websocketHandlers/WebsocketServices.java
@@ -1,4 +1,25 @@
 package websocketHandlers;
 
+import chess.ChessMove;
+import model.GameData;
+import dataAccess.AuthDAO;
+import dataAccess.GameDAO;
+import dataAccess.SQLAuthDAO;
+import dataAccess.SQLGameDAO;
+
 public class WebsocketServices {
+    private final AuthDAO authDAO = new SQLAuthDAO();
+    private final GameDAO gameDAO = new SQLGameDAO();
+
+    public GameData leaveGame(int gameID, String color, String authToken) {
+        return new GameData(-1, null, null, null, null);
+    }
+
+    public GameData makeMove(int gameID, ChessMove move, String authToken) {
+        return new GameData(-1, null, null, null, null);
+    }
+
+    public GameData resign(int gameID, String color, String authToken) {
+        return new GameData(-1, null, null, null, null);
+    }
 }
